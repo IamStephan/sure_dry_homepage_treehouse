fd33c9172e67ea4193f5951092a252fad9bd9ffc
iamstephan <stephanburger54@gmail.com>
2021-05-28 02:04:02 +0200

    tailwind and custom plugin

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: ["./*.html"],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Montserrat", "serif"],
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#c8102e",
+        secondary: "#ffd00a",
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/typography")],
+};
