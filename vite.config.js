198d6884495524f884ff3b59f685d0009901e86b
iamstephan <stephanburger54@gmail.com>
2021-05-30 17:33:45 +0200

    image compression

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,38 @@
+import viteImagemin from "vite-plugin-imagemin";
+
 import { svgLoader } from "./custom_plugins/svg_loader";
 
 export default {
-  plugins: [svgLoader()],
+  plugins: [
+    svgLoader(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      webp: {
+        quality: 75,
+      },
+      mozjpeg: {
+        quality: 65,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+          {
+            removeEmptyAttrs: false,
+          },
+        ],
+      },
+    }),
+  ],
 };
