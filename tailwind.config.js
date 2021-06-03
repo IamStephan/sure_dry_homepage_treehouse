7a62073a2767bbe2bb0e8a02c920004561fdf47b
iamstephan <stephanburger54@gmail.com>
2021-06-03 14:58:57 +0200

    added aspect ratios for media content

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,5 +47,8 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
